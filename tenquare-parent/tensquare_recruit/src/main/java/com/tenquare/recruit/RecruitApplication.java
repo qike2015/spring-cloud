83676165c0461024bb1ca4e120015e2b9670833c
qike2015 <649381130@qq.com>
2018-12-25 23:56:34 +0800

    fix(tensquare-recriut)初始化

---
+++ tenquare-parent/tensquare_recruit/src/main/java/com/tenquare/recruit/RecruitApplication.java
@@ -0,0 +1,23 @@
+package com.tenquare.recruit;
+
+import com.qike.tensquare.util.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RecruitApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RecruitApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorkker()
+    {
+        return new IdWorker(1, 1);
+    }
+
+}
---
+++ tenquare-parent/tensquare_recruit/src/main/java/com/tenquare/recruit/controller/BaseExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.tenquare.recruit.controller;
+
+import com.qike.tensquare.entity.Result;
+import com.qike.tensquare.entity.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * 统一异常处理类
+ */
+@ControllerAdvice
+public class BaseExceptionHandler
+{
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result error(Exception e)
+    {
+        e.printStackTrace();
+        return new Result(false, StatusCode.ERROR, "执行出错");
+    }
+}
