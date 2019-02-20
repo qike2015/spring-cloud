7ffd6c9e4457b5e05372c7e04dea551c6d04e5dd
qike2015 <649381130@qq.com>
2019-02-20 22:28:54 +0800

    网关认证

--- tenquare-parent/tensquare_manager/src/main/java/com/tensquare/manager/ManagerApplication.java
+++ tenquare-parent/tensquare_manager/src/main/java/com/tensquare/manager/ManagerApplication.java
@@ -3,6 +3,8 @@ package com.tensquare.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import util.JwtUtil;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,5 +13,10 @@ public class ManagerApplication
     public static void main(String[] args)
     {
         SpringApplication.run(ManagerApplication.class,args);
+    }
+
+    @Bean
+    public JwtUtil jwtUtil(){
+        return new JwtUtil();
     }
 }
