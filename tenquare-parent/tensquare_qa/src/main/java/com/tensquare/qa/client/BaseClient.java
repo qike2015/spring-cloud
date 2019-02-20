99dbcc991dff8f8e3bd52c3936c51d5a3ae382d5
qike2015 <649381130@qq.com>
2019-02-20 16:27:00 +0800

    熔断器

--- tenquare-parent/tensquare_qa/src/main/java/com/tensquare/qa/client/BaseClient.java
+++ tenquare-parent/tensquare_qa/src/main/java/com/tensquare/qa/client/BaseClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("tensquare-base")
+@FeignClient(value = "tensquare-base",fallback = BaseClientImpl.class)
 public interface BaseClient
 {
 
